6ea2c60807fa6a36ad21041fbe8d5503179db5d6
Caleb Rogers <rogersjcaleb@gmail.com>
2016-01-20 16:30:39 -0600

    for now put everything into calculateTrianglePerimeter()

---
+++ javascriptExamples/calculateTriangle/measureTriangle.js
@@ -0,0 +1,36 @@
+/*
+later, I want it to take a calculator argument so I can have a list of different
+calculators that open up
+*/
+function openCalculator(){
+  document.getElementById("triangle").innerHTML =
+  '<form id="triangleform"> ' +
+  '<br>First side:  <input type="text" name="side1"><br>Second Side <input type="text" name="side2"> <br>Third Side <input type="text" name="side3"><br>'
+  + '<br><input type="button" name="button" value="Click" onClick="calculateTrianglePerimeter(this.form)"></form>'
+}
+
+
+
+function calculateTrianglePerimeter(form){
+  var side1 = parseInt(form.side1.value);
+  var side2 = parseInt(form.side2.value);
+  var side3 = parseInt(form.side3.value);
+  var perimeter = (side1 + side2 + side3);
+  alert(perimeter);
+}
+
+
+/*
+function getFormValue(form){
+  var object = {
+    side1: form.side1.value;
+    side2: form.side2.value;
+    side3: form.side3.value;
+  };
+  return object;
+}*/
+/*
+function getSideValue(){
+  var side2 = document.forms["triangleform"]["side2"].value;
+  alert(side2);
+}*/
