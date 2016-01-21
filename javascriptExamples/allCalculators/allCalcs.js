52d8ec2bc9f2e02c23a2a0a361881c226f92e40c
Caleb Rogers <rogersjcaleb@gmail.com>
2016-01-21 17:33:14 -0600

    added square area calculator

--- javascriptExamples/allCalculators/allCalcs.js
+++ javascriptExamples/allCalculators/allCalcs.js
@@ -16,18 +16,28 @@ window.onclick = function(event){
   }
 }
 
-//this should make something appear below the dropdown menu
+//have the chosen calculator appear below the dropdown menu
 function showCalc(element){
   iFrameName = document.getElementById(String(element.id) + "Calculator");
   iFrameContent = iFrameName.contentDocument.body.innerHTML;
   document.getElementById("originalContent").innerHTML= iFrameContent;
 }
 
+//TODO: find a way to split apart the "printing" part of these functions
 function calculateTrianglePerimeter(form){
   var side1 = parseInt(form.side1.value);
   var side2 = parseInt(form.side2.value);
   var side3 = parseInt(form.side3.value);
+
   var perimeter = (side1 + side2 + side3);
   document.getElementById("originalContent").innerHTML = 'Your triangle perimeter is ' + perimeter + '.';
+}
 
+
+function calculateSquareArea(form){
+  var side1 = parseInt(form.side1.value);
+  var side2 = parseInt(form.side2.value);
+  var area = (side1 * side2);
+
+  document.getElementById("originalContent").innerHTML = 'Your square area is ' + area + '.';
 }
