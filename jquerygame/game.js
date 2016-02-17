57284b918c770184cda730d8440fee9eda609ba8
Caleb Rogers <rogersjcaleb@gmail.com>
2016-02-16 19:27:46 -0800

    circle drawn

--- jquerygame/game.js
+++ jquerygame/game.js
@@ -1,2 +1,9 @@
-var canvas = document.getElementById('clock');
-var context = canvas.getContext('2d');
+
+
+var draw = function(){
+  var canvas = document.getElementById('clock');
+  var context = canvas.getContext('2d');
+  context.arc(75,75,50,0, 2 * Math.PI, false);
+  context.fillStyle = "blue";
+  context.fill();
+}
