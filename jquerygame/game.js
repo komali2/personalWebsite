efca2a6fb03b2abe2bfa56f0821f6bbe2c779507
Caleb Rogers <rogersjcaleb@gmail.com>
2016-02-17 10:06:47 -0800

    got the hour lines working

--- jquerygame/game.js
+++ jquerygame/game.js
@@ -7,28 +7,28 @@ var draw = function(){
   context.fillStyle = "blue";
   context.fill();
   var step = 2*Math.PI / 12;
-  var xCord = 75;
-  var yCord = 75;
+  var xCenter = 75;
+  var yCenter = 75;
   var r = 50;
-  var small = 0.5;
+  var small = 0.8;
+  var big = 1.2;
   //context.beginPath();
   for(var theta = 0; theta < 2*Math.PI; theta += step){
-    var x = xCord + r * Math.cos(theta);
-    var y = yCord - r * Math.sin(theta);
-    context.moveTo(x,y);
-    var smallx = (xCord + (r*small) * Math.cos(theta));
-    var smally = (yCord - (r*small) * Math.sin(theta));
+    var bigx = xCenter + (r*big) * Math.cos(theta);
+    var bigy = yCenter - (r*big) * Math.sin(theta);
+    var smallx = (xCenter + (r*small) * Math.cos(theta));
+    var smally = (yCenter - (r*small) * Math.sin(theta));
     context.fillStyle = "red";
-    context.fillRect(smallx, smally, 10, 10);
     context.beginPath();
+    context.moveTo(bigx,bigy);
     context.lineTo(smallx, smally);
-    context.closePath();
     context.stroke();
+    context.closePath();
   }
   //context.closePath();
   //context.stroke();
 };
 
 var drawHashes = function(context){
-  context.moveTo(125,75);
+  
 };
