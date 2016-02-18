de4b067d9471066a140731564223d875b9c9ca5b
komali2 <rogersjcaleb@gmail.com>
2016-02-17 16:19:18 -0800

    added css, oh god why doesn't any of this work

--- drawanimation/drawanimation.js
+++ drawanimation/drawanimation.js
@@ -1,12 +1,18 @@
+var width = 500;
+var height = 500;
+
 var draw = function(){
   var date = new Date();
   var canvas = document.getElementById('drawer');
   var context = canvas.getContext('2d');
   context.save();
-  //context.clearRect(0,0,500,500);
-  context.translate(250,250);
-  context.rotate(-Math.PI/2);
-
+  context.clearRect(0,0,width,height);
+  context.translate(width/2,height/2);
+  //context.rotate(-Math.PI/2);
+  var seconds = date.getSeconds();
   context.fillStyle = 'orange';
-  context.fillRect(0,0,100,100);
+  context.fillRect(150, 150 ,100,100);
+
+  window.requestAnimationFrame(draw);
+  context.restore();
 };
