5cb62f9e10000600643978d406b44679e8b3450c
komali2 <rogersjcaleb@gmail.com>
2016-08-01 15:05:34 -0700

    core: appending using string interpolation

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,5 @@
 $(".portfolio :button").click(function(){
-    var test = $(this)[0].dataset.name;
-    console.log(test);
-    var test2 = $("<div>Hello</div>");
-    $(`.portfolio .${test}`).append(test2);
-    $('.portfolio.gitnalysis').append(test2);
+    var name = $(this)[0].dataset.name;
+    $(`.portfolio.${name}`).append('hello');
     
 })
