ab0f3fa8ed8bd26fdfb3b78923993e1f596ce9a1
komali2 <rogersjcaleb@gmail.com>
2016-08-24 19:43:32 -0700

    core: changing text on scroll to certain height

--- index2.js
+++ index2.js
@@ -3,6 +3,13 @@
 $(document).ready(()=>{
     $(window).scroll(()=>{
         console.log('scrolled', $(window).scrollTop());
+        if($(window).scrollTop() >= 200){
+            $('.whenBig').toggle(false);
+            $('.whenSmall').toggle(true);
+        } else{
+            $('.whenBig').toggle(true);
+            $('.whenSmall').toggle(false);
+        }
     })
 });
 
