105f53bd3a344e4953aff14840d7c7f3b97feab8
komali2 <rogersjcaleb@gmail.com>
2016-08-25 14:31:11 -0700

    bp: moving away from animations for now

--- index2.js
+++ index2.js
@@ -3,12 +3,15 @@
 $(document).ready(()=>{
     $(window).scroll(()=>{
         console.log('scrolled', $(window).scrollTop());
-        if($(window).scrollTop() >= 200){
+        if($(window).scrollTop() >= 100){
             $('.whenBig').toggle(false);
             $('.whenSmall').toggle(true);
+            $('.banner').css('flex-basis', '100px');
+            $('.banner').addClass('small');
         } else{
             $('.whenBig').toggle(true);
             $('.whenSmall').toggle(false);
+            // $('.banner').css('flex-basis', '400px');
         }
     })
 });
