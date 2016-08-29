6e98fd743a2e13dc47d28d28eaf3264c340fbf0c
komali2 <rogersjcaleb@gmail.com>
2016-08-29 13:01:11 -0700

    des: pop down boxes on resize too small

--- index2.js
+++ index2.js
@@ -4,13 +4,13 @@ $(document).ready(()=>{
     $(window).scroll(()=>{
         console.log('scrolled', $(window).scrollTop());
         if($(window).scrollTop() >= 100){
-            $('.whenBig').toggle(false);
-            $('.whenSmall').toggle(true);
-            $('.banner').css('flex-basis', '100px');
-            $('.banner').addClass('small');
+            // $('.whenBig').toggle(false);
+            // $('.whenSmall').toggle(true);
+            // $('.banner').css('flex-basis', '100px');
+            // $('.banner').addClass('small');
         } else{
-            $('.whenBig').toggle(true);
-            $('.whenSmall').toggle(false);
+            // $('.whenBig').toggle(true);
+            // $('.whenSmall').toggle(false);
             // $('.banner').css('flex-basis', '400px');
         }
     })
