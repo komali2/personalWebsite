458d1cdd4f5009b8a653cac4e3ae5da5aee495e1
komali2 <rogersjcaleb@gmail.com>
2016-09-07 19:24:20 -0700

    dev: final cleanup

--- portfolio.js
+++
@@ -1,6 +0,0 @@
-$(".portfolio :button").click(function(){
-    var name = $(this)[0].dataset.name;
-    $(`.portfolio.${name}`).append($(`.info.${name}`));
-    $(`.info.${name}`).toggle();
-    
-})
