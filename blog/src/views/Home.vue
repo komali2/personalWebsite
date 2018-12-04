8b6e8f2e8d4c30f87126fcd25f4a5d3aad2aa062
Caleb Rogers <rogersjcaleb@gmail.com>
2018-12-03 17:53:47 -0800

    Get the first page somewhat sorted

--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="home">
-    
-  </div>
+  <main class="home">
+    <header>
+      <h1>
+        <small>This is</small>
+        Caleb's Blog
+      </h1>
+      <h3>
+        <small>
+          Updated: <time datetime="2018-12-3">Dec, 2018</time>
+        </small>
+      </h3>
+    </header>
+  </main>
 </template>
 
 <script lang="ts">
