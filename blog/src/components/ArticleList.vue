85a1147a17f7d465bf1da770a249cc0c5633629e
Caleb Rogers <rogersjcaleb@gmail.com>
2018-12-03 18:05:21 -0800

    Get a skeleton articlelist component

---
+++ blog/src/components/ArticleList.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="article-list">
+    <h1>This is an article list page</h1>
+  </div>
+</template>
--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -11,14 +11,22 @@
         </small>
       </h3>
     </header>
+    <div>
+      <h1>
+        Articles:
+      </h1>
+      <article-list/>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ArticleList from '@/components/ArticleList.vue';
 
 @Component({
   components: {
+    ArticleList
   },
 })
 export default class Home extends Vue {}
